e6b7ab7067ce1287cf22505a24ba0ce9cf2ec730
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-06-07 10:41:12 +0000

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import reactX from "eslint-plugin-react-x";
-import reactDom from "eslint-plugin-react-dom";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -33,8 +31,6 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "react-x": reactX,
-      "react-dom": reactDom,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -42,9 +38,6 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
-      ...reactX.configs["recommended-typescript"]
-        .rules,
-      ...reactDom.configs.recommended.rules,
     },
   },
 );
