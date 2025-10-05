df4b6d75b10fda3d029740002443e808f8daeb4d
Dkotl <126883337+Dkotl@users.noreply.github.com>
2025-10-05 17:27:54 +0000

    recreate progect

--- eslint.config.js
+++ eslint.config.js
@@ -3,41 +3,21 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default defineConfig([
+  globalIgnores(["dist"]),
   {
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
     ],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        project: [
-          "./tsconfig.node.json",
-          "./tsconfig.app.json",
-        ],
-      },
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
     },
   },
-);
+]);
